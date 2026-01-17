6ba434811d47062905edc4b8964b982a9c6719db
Vansh Pratap Singh Jadon <vanshpratapsinghjadon95@gmail.com>
2026-01-17 22:30:34 +0530

    Update AboutSection.jsx

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -49,7 +49,7 @@ export const AboutSection = () => {
                   <Code className="h-6 w-6 text-primary" />
                 </div>
                 <div className="text-left">
-                  <h4 className="font-semibold text-lg"> ML Developer</h4>
+                  <h4 className="font-semibold text-lg"> AI/ML Developer</h4>
                   <p className="text-muted-foreground">
                     Creating responsive models and ml applications with
                     modern frameworks.
@@ -63,7 +63,7 @@ export const AboutSection = () => {
                   <User className="h-6 w-6 text-primary" />
                 </div>
                 <div className="text-left">
-                  <h4 className="font-semibold text-lg">UI/UX Design</h4>
+                  <h4 className="font-semibold text-lg">UI/UX Designer</h4>
                   <p className="text-muted-foreground">
                     Designing intuitive user interfaces and seamless user
                     experiences.
